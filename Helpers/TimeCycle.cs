dc536eb25b6b3ff73d426231115094d66d36ca89
obrienu <obrienules@gmail.com>
2021-02-04 08:52:15 +0100

    Adds adhoc to TimeCycle

--- Helpers/TimeCycle.cs
+++ Helpers/TimeCycle.cs
@@ -2,6 +2,6 @@ namespace HaloBiz.Helpers
 {
     public enum TimeCycle
     {
-        Weekly,BiWeekly, Monthly, BiMonthly, Quarterly, SemiAnnually, Annually, BiAnnually, Others
+        Weekly,BiWeekly, Monthly, BiMonthly, Quarterly, SemiAnnually, Annually, BiAnnually, Others,Adhoc
     }
 }
