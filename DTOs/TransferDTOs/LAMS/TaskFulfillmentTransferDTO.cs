ea19b7702ca19f853911a2f1c5e1842fdd2190ae
Umoren Joshua <umorenjoshua50@gmail.com>
2021-02-05 21:47:49 +0100

    Added Transfer DTO for Task Fulfillment

---
+++ DTOs/TransferDTOs/LAMS/TaskFulfillmentTransferDTO.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using HaloBiz.Model;
+using HaloBiz.Model.LAMS;
+
+namespace HaloBiz.DTOs.TransferDTOs.LAMS
+{
+    public class TaskFulfillmentTransferDTO
+    {
+        public long Id { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public long CustomerDivisionId { get; set; }
+        public CustomerDivision CustomerDivision { get; set; }
+        public long ContractServiceId { get; set; }
+        public ContractService ContractService { get; set; }
+        public long? ResponsibleId { get; set; }
+        public UserProfile Responsible { get; set; }
+        public long? AccountableId { get; set; }
+        public UserProfile Accountable { get; set; }
+        public long? ConsultedId { get; set; }
+        public UserProfile Consulted { get; set; }
+        public long? InformedId { get; set; }
+        public UserProfile Informed { get; set; }
+        public long? SupportId { get; set; }
+        public UserProfile Support { get; set; }
+        public double? Budget { get; set; }
+        public bool IsPicked { get; set; } = false;
+        public DateTime? DateTimePicked { get; set; }
+        public bool IsAllDeliverableAssigned { get; set; } = false;
+        public DateTime? TaskCompletionDateTime { get; set; }
+        public bool TaskCompletionStatus { get; set; } = false;
+        public long CreatedById { get; set; }
+        public virtual UserProfile CreatedBy { get; set; }
+    }
+}
