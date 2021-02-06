70d31d513344c4b271e0f78ec692393fe824d1bf
obrienu <obrienules@gmail.com>
2021-02-06 20:02:29 +0100

    Update LeadKeyPerson

--- DTOs/TransferDTOs/LAMS/LeadKeyPersonTransferDTO.cs
+++ DTOs/TransferDTOs/LAMS/LeadKeyPersonTransferDTO.cs
@@ -2,6 +2,7 @@ namespace HaloBiz.DTOs.TransferDTOs.LAMS
 {
     public class LeadKeyPersonTransferDTO : ContactTransferDTO
     {
-        
+        public long LeadId { get; set; }
+        public virtual LeadWithoutModelsTransferDTO Lead { get; set; }
     }
 }
