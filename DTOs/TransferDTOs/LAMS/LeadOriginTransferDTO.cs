1b7c850ea970ffc6d82fb3862e9b263414fac2db
obrienu <obrienules@gmail.com>
2021-02-06 13:16:02 +0100

    Updated LeadType and LeadOrigin repo and DTOs

--- DTOs/TransferDTOs/LAMS/LeadOriginTransferDTO.cs
+++ DTOs/TransferDTOs/LAMS/LeadOriginTransferDTO.cs
@@ -4,9 +4,18 @@ namespace HaloBiz.DTOs.TransferDTOs.LAMS
 {
     public class LeadOriginTransferDTO
     {
-        [Key]
         public long Id { get; set; }
         public string Caption { get; set; }
         public string Description { get; set; }
+        public long LeadTypeId { get; set; }
+        public LeadTypeWithoutOriginDTO LeadType { get; set; }
+    }
+
+    public class LeadTypeWithoutOriginDTO
+    {
+        public long Id { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+
     }
 }
