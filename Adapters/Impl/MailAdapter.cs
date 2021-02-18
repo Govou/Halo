e66e898ab8877d7999cf31489fc7ac43ce783185
Jayora50 <umorenjoshua50@gmail.com>
2021-02-18 19:13:40 +0100

    Added Implemetation for adapter used in mail integration with Mail Microservice

---
+++ Adapters/Impl/MailAdapter.cs
@@ -0,0 +1,44 @@
+﻿using HaloBiz.DTOs.ApiDTOs;
+using Flurl;
+using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace HaloBiz.Adapters.Impl
+{
+    public class MailAdapter : IMailAdapter
+    {
+        private readonly ILogger<MailAdapter> _logger;
+        public MailAdapter(ILogger<MailAdapter> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<ApiResponse> SendUserAssignedToRoleMail(string userEmail, string userName)
+        {
+            var baseUrl = "http://halobiz-mail-api.herokuapp.com/";
+
+            try
+            {
+                var response = await baseUrl.AllowAnyHttpStatus()
+               .AppendPathSegments("/Mail​/SendSampleNotification")
+               .PostJsonAsync(new
+               {
+                   emailAddress = userEmail,
+                   userName
+               }).ReceiveJson<object>();
+
+                return new ApiOkResponse(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.Message);
+                _logger.LogInformation(ex.StackTrace);
+                return new ApiResponse(500, ex.Message);
+            }
+        }
+    }
+}
