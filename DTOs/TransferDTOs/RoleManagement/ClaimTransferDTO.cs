f66a160a34daf730258cf1b75fce5ff50dbfe5d4
Jayora50 <umorenjoshua50@gmail.com>
2021-02-18 17:05:20 +0100

    Added DTO for Claims

---
+++ DTOs/TransferDTOs/RoleManagement/ClaimTransferDTO.cs
@@ -0,0 +1,15 @@
+﻿using HaloBiz.Model.RoleManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.DTOs.TransferDTOs.RoleManagement
+{
+    public class ClaimTransferDTO
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+    }
+}
