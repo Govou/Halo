83e09fbd66c1b7c9ed1e6ac549f78d1e7f929980
Jayora50 <umorenjoshua50@gmail.com>
2021-02-19 05:27:43 +0100

    took out role name validation

--- DTOs/ReceivingDTOs/RoleManagement/RoleReceivingDTO.cs
+++ DTOs/ReceivingDTOs/RoleManagement/RoleReceivingDTO.cs
@@ -10,7 +10,6 @@ namespace HaloBiz.DTOs.ReceivingDTOs.RoleManagement
     public class RoleReceivingDTO
     {
         [Required]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Role name should only contain characters with no white space"), MaxLength(40)]
         public string Name { get; set; }
 
         [Required]
