a9cf7039f6db415a56dd80ad72ca5867ff6eb49b
Umoren Joshua <umorenjoshua50@gmail.com>
2021-02-24 11:41:35 +0100

    Added Approver Level Model

---
+++ Model/ApproverLevel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.Model
+{
+    public class ApproverLevel
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required, MinLength(3), MaxLength(50)]
+        public string Caption { get; set; }
+        [Required, MinLength(3), MaxLength(255)]
+        public string Description { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
