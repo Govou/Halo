5d05dfcb5dc76c19b206d83b926c7d1eb060277c
Umoren Joshua <umorenjoshua50@gmail.com>
2021-02-24 15:30:36 +0100

    Minor update

--- DTOs/TransferDTOs/ApprovalLimitTransferDTO.cs
+++ DTOs/TransferDTOs/ApprovalLimitTransferDTO.cs
@@ -5,11 +5,11 @@ namespace HaloBiz.DTOs.TransferDTOs
     public class ApprovalLimitTransferDTO : BaseSetupTransferDTO
     {
         public long ProcessesRequiringApprovalId { get; set; } //module captured
-        public virtual ProcessesRequiringApproval ProcessesRequiringApproval { get; set; }
+        public  ProcessesRequiringApproval ProcessesRequiringApproval { get; set; }
         public long UpperlimitValue { get; set; }
         public long LowerlimitValue { get; set; }
         public long ApproverLevelId { get; set; }
-        public virtual ApproverLevel ApproverLevel { get; set; }
+        public  ApproverLevel ApproverLevel { get; set; }
         public long Sequence { get; set; }
         public bool IsBypassRequired { get; set; } = false;
 
