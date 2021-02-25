a3ece9efd48b5b4139cb48c3dfcb4d5cfe2a1926
Joshua Umoren <umorenjoshua50@gmail.com>
2021-02-25 16:24:31 +0100

    Added Company Model and DataContext

--- Model/Company.cs
+++ Model/Company.cs
@@ -1,23 +1,25 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HaloBiz.Model
 {
-    public class Division
+    public class Company
     {
         [Key]
         public long Id { get; set; }
-        [Required, MinLength(3), MaxLength(100)]
+        [Required, MinLength(3), MaxLength(50)]
         public string Name { get; set; }
         [Required, MinLength(3), MaxLength(255)]
         public string Description { get; set; }
-        public string MissionStatement { get; set; }
-        public long HeadId { get; set; }
-        public long CompanyId { get; set; }
+        [Required, MaxLength(500, ErrorMessage = "Requires a maximum of 500 characters")]
+        public string Address { get; set; }
+        public long? HeadId { get; set; }
         public virtual UserProfile Head { get; set; }
-        public IEnumerable<OperatingEntity> OperatingEntities { get; set; }
+        public IEnumerable<Division> Divisions { get; set; }
         public bool IsDeleted { get; set; }
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreatedAt { get; set; }
