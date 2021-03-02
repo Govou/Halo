7cbad18c754638938ec0f8a786660f629f8faceb
obrienu <obrienules@gmail.com>
2021-03-02 11:43:35 +0100

    Updates customer dto

--- DTOs/TransferDTOs/LAMS/CustomerTransferDTO.cs
+++ DTOs/TransferDTOs/LAMS/CustomerTransferDTO.cs
@@ -16,6 +16,12 @@ namespace HaloBiz.DTOs.TransferDTOs.LAMS
         public string RCNumber { get; set; }
         public long GroupTypeId { get; set; }
         public  GroupTypeTransferDTO GroupType { get; set; }
+        public LeadContactTransferDTO PrimaryContact { get; set; }
+        public List<LeadKeyPersonTransferDTO> KeyPeople { get; set; } 
+        public long? PrimaryContactId { get; set; }   
+        public LeadContactTransferDTO SecondaryContact { get; set; }
+        public long? SecondaryContactId { get; set; } 
+
         public IEnumerable<CustomerDivisionTransferDTO> CustomerDivisions { get; set; }
     }
 }
