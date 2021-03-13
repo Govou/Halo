651172384dbeaf5fe76617d51671cfe874f7423e
Joshua Umoren <umorenjoshua50@gmail.com>
2021-03-13 11:19:06 +0100

    Added Mode of Transport to data context

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -93,6 +93,7 @@ namespace HaloBiz.Data
     public DbSet<GroupInvoiceDetails> GroupInvoiceDetails { get; set; }
     public DbSet<GroupInvoiceTracker> GroupInvoiceTracker { get; set; }
     public DbSet<SBUProportion> SBUProportions { get; set; }
+    public DbSet<ModeOfTransport> ModeOfTransports { get; set; }
     
     public DbSet<EndorsementTypeTracker> EndorsementTypeTrackers { get; set; }
     public DbSet<ContractServiceForEndorsement> ContractServiceForEndorsements{ get; set; }
@@ -720,6 +721,14 @@ namespace HaloBiz.Data
                .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
             builder.Entity<ContractServiceForEndorsement>()
+                .Property(p => p.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Entity<ModeOfTransport>()
+              .Property(p => p.UpdatedAt)
+              .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Entity<ModeOfTransport>()
                 .Property(p => p.CreatedAt)
                 .HasDefaultValueSql("CURRENT_TIMESTAMP");
         }
