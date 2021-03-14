d851fd60cec1f5c21a93044983dbaec58543cbed
Jayora50 <umorenjoshua50@gmail.com>
2021-03-14 12:01:24 +0100

    added migrations for Account IntegrationFlag

---
+++ Migrations/20210314110049_extendedAccountModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HaloBiz.Migrations
+{
+    public partial class extendedAccountModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IntegrationFlag",
+                table: "Accounts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IntegrationFlag",
+                table: "Accounts");
+        }
+    }
+}
