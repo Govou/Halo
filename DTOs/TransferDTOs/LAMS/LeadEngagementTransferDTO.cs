b9322e9f7c26b64095788cb49654bee8f251038d
Ahmed Sulaiman <ahmed.sulaiman@halogen-group.com>
2021-04-21 01:54:48 +0100

    various changes

--- DTOs/TransferDTOs/LAMS/LeadEngagementTransferDTO.cs
+++ DTOs/TransferDTOs/LAMS/LeadEngagementTransferDTO.cs
@@ -13,12 +13,14 @@ namespace HaloBiz.DTOs.TransferDTOs.LAMS
         public string EngagementDiscussion { get; set; }
         public long EngagementTypeId { get; set; }
         public EngagementType EngagementType { get; set; }
+        public long EngagementReasonId { get; set; }
+        public EngagementReason EngagementReason { get; set; }
         public DateTime Date { get; set; }
         public long LeadId { get; set; }
         public Lead Lead { get; set; }
         public string LeadCaptureStage { get; set; }
         public string EngagementOutcome { get; set; }
-        public string ReasonForEngagement { get; set; }
+        public string DocumentsUrl { get; set; }
         public ICollection<LeadDivisionContactLeadEngagementTransferDTO> LeadDivisionContactLeadEngagements { get; set; }
         public ICollection<LeadDivisionKeyPersonLeadEngagementTransferDTO> LeadDivisionKeyPersonLeadEngagements { get; set; }
         public ICollection<LeadEngagementUserProfileTransferDTO> LeadEngagementUserProfiles { get; set; }
