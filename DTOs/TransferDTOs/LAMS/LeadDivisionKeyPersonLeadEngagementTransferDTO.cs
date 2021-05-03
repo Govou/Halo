1c9f83b1993a584683d32de937aa295ea7fef144
Ahmed Sulaiman <ahmed.sulaiman@halogen-group.com>
2021-05-03 09:56:41 +0100

    various changes

--- DTOs/TransferDTOs/LAMS/LeadDivisionKeyPersonLeadEngagementTransferDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HaloBiz.DTOs.TransferDTOs.LAMS
-{
-    public class LeadDivisionKeyPersonLeadEngagementTransferDTO
-    {
-        public long KeyPersonsEngagedWithId { get; set; }
-        public long LeadEngagementsId { get; set; }
-    }
-}
--- DTOs/TransferDTOs/LAMS/LeadEngagementUserProfileTransferDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HaloBiz.DTOs.TransferDTOs.LAMS
-{
-    public class LeadEngagementUserProfileTransferDTO
-    {
-        public long UserLeadEngagementsId { get; set; }
-        public long UsersEngagedWithId { get; set; }
-    }
-}
