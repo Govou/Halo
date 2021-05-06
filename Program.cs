d463439d8c66257dbd2afae42eb51a8f05c60bfd
Adebayo Salami <salamibolarinwa16@gmail.com>
2021-05-06 14:09:48 +0100

    Bug Fix with escalation matrix

--- Program.cs
+++ Program.cs
@@ -20,11 +20,13 @@ namespace HaloBiz
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
-                    .UseKestrel(options =>
-                    {
-                        options.ListenAnyIP(Int32.Parse(System.Environment.GetEnvironmentVariable("PORT") ?? "5050"));
-                    });
+                    webBuilder.UseStartup<Startup>();       //To run locally
+
+                    //webBuilder.UseStartup<Startup>()        //When pushing
+                    //.UseKestrel(options =>
+                    //{
+                    //    options.ListenAnyIP(Int32.Parse(System.Environment.GetEnvironmentVariable("PORT") ?? "5050"));
+                    //});
                 });
     }
 }
