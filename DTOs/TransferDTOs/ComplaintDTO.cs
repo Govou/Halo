f8f670039c6c0c6a69eacd1ac4693354e7f23cf3
Adebayo Salami <salamibolarinwa16@gmail.com>
2021-05-06 15:06:42 +0100

    Removing Default COnnection string (Security Reasons)

---
+++ DTOs/TransferDTOs/ComplaintDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.DTOs.TransferDTOs
+{
+    public class ComplaintDTO
+    {
+    }
+    public class ComplaintHandlingDTO
+    {
+        public List<ComplaintDTO> assignedComplaints { get; set; }
+        public List<ComplaintDTO> workbenchComplaints { get; set; }
+    }
+
+    public class ComplaintHandlingStatsDTO
+    {
+        public int TotalComplaintsAssigned { get; set; }
+        public int TotalComplaintsInWorkbench { get; set; }
+        public int TotalComplaintsBeingHandled { get; set; }
+        public int ToalComplaintsClosed { get; set; }
+    }
+}
---
+++ Repository/IComplaintHandlingRepository.cs
@@ -0,0 +1,14 @@
+﻿using HaloBiz.DTOs.TransferDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.Repository
+{
+    public interface IComplaintHandlingRepository
+    {
+        Task<ComplaintHandlingStatsDTO> GetComplaintHandlingStats(long userProfileID);
+        Task<ComplaintHandlingDTO> GetComplaintsHandling(long userProfileID);
+    }
+}
