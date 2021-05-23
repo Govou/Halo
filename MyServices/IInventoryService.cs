a666fb709108801f4bc0663ee0742e26319c1763
Adebayo Salami <salamibolarinwa16@gmail.com>
2021-05-23 21:44:57 +0100

    Commit changes

---
+++ MyServices/IInventoryService.cs
@@ -0,0 +1,16 @@
+﻿using HaloBiz.DTOs.ApiDTOs;
+using HaloBiz.DTOs.ReceivingDTOs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.MyServices
+{
+    public interface IInventoryService
+    {
+        Task<ApiResponse> CreateInventoryItem(HttpContext context);
+        Task<ApiResponse> CreateInventoryItemGroup(HttpContext context);
+    }
+}
---
+++ MyServices/Impl/InventoryServiceImpl.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HaloBiz.DTOs.ApiDTOs;
+using HaloBiz.DTOs.ReceivingDTOs;
+using HaloBiz.DTOs.TransferDTOs;
+using HaloBiz.Helpers;
+using HalobizMigrations.Models;
+using HaloBiz.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HalobizMigrations.Models.Complaints;
+using HalobizMigrations.Data;
+using Microsoft.EntityFrameworkCore;
+using HaloBiz.DTOs.MailDTOs;
+
+namespace HaloBiz.MyServices.Impl
+{
+    public class InventoryServiceImpl : IInventoryService
+    {
+        private readonly HalobizContext _context;
+        private readonly ILogger<InventoryServiceImpl> _logger;
+        private readonly IMapper _mapper;
+        public InventoryServiceImpl(HalobizContext context, ILogger<ComplaintHandlingServiceImpl> logger, IMapper mapper)
+        {
+            _context = context;
+            _logger = logger;
+            _mapper = mapper;
+        }
+    }
+}
