c9ad74e093307c3c5edce0f29ef5eccb45c22b88
Ahmed Sulaiman <ahmed.sulaiman@halogen-group.com>
2021-06-02 05:04:57 +0100

    changes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,7 +7,9 @@ using HaloBiz.MyServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -25,88 +27,120 @@ namespace HaloBiz.Controllers
 
         private readonly IUserProfileService userProfileService;
         private readonly IConfiguration _config;
+        private readonly ILogger<AuthController> _logger;
 
         public AuthController(
             IUserProfileService userProfileService,
-            IConfiguration config)
+            IConfiguration config,
+            ILogger<AuthController> logger)
         {
             this._config = config;
             this.userProfileService = userProfileService;
+            _logger = logger;
         }
 
         [AllowAnonymous]
         [HttpPost("Login")]
         public async Task<ActionResult> Login(LoginReceivingDTO loginReceiving)
         {
-            GoogleJsonWebSignature.Payload payload;
-
             try
             {
-                payload = await GoogleJsonWebSignature.ValidateAsync(loginReceiving.IdToken);
+                GoogleJsonWebSignature.Payload payload;
+
+                try
+                {
+                    payload = await GoogleJsonWebSignature.ValidateAsync(loginReceiving.IdToken);
+                }
+                catch (InvalidJwtException invalidJwtException)
+                {
+                    _logger.LogWarning($"Could not validate Google Id Token {loginReceiving.IdToken} => {invalidJwtException.Message}");
+                    return StatusCode(404, invalidJwtException.Message);
+                }
+
+                if (!payload.EmailVerified)
+                {
+                    _logger.LogWarning($"Email verification failed. Payload => {JsonConvert.SerializeObject(payload)}");
+                    return StatusCode(404, "Email verification failed.");
+                }
+
+                var email = payload.Email;
+
+                var response = await userProfileService.FindUserByEmail(email);
+                if (response.StatusCode >= 400)
+                {
+                    _logger.LogWarning($"Could not find user [{email}] => {response.Message}");
+                    return StatusCode(response.StatusCode, response);
+                }
+                    
+                var user = ((ApiOkResponse)response).Result;
+                var userProfile = (UserProfileTransferDTO)user;
+
+                var jwtToken = GenerateToken(userProfile);
+                return Ok(new UserAuthTransferDTO { Token = jwtToken, UserProfile = userProfile });
             }
-            catch (InvalidJwtException invalidJwtException)
+            catch (Exception ex)
             {
-                return StatusCode(404, invalidJwtException.Message);
-            }
-
-            if (!payload.EmailVerified)
-            {
-                return StatusCode(404, "Email verification failed.");
-            }
-
-            var email = payload.Email;
-
-            var response = await userProfileService.FindUserByEmail(email);
-            if (response.StatusCode >= 400)
-                return StatusCode(response.StatusCode, response);
-            var user = ((ApiOkResponse)response).Result;
-            var userProfile = (UserProfileTransferDTO)user;
-
-            var jwtToken = GenerateToken(userProfile);
-            return Ok(new UserAuthTransferDTO { Token = jwtToken, UserProfile = userProfile });
+                _logger.LogError(ex.Message);
+                _logger.LogError(ex.StackTrace);
+                return StatusCode(500, $"An error occured, {ex.Message}");
+            }       
         }
 
         [AllowAnonymous]
         [HttpPost("CreateUser")]
         public async Task<ActionResult> CreateProfile(AuthUserProfileReceivingDTO authUserProfileReceivingDTO)
         {
-
-            GoogleJsonWebSignature.Payload payload;         
-
             try
             {
-                payload = await GoogleJsonWebSignature.ValidateAsync(authUserProfileReceivingDTO.IdToken);
+                GoogleJsonWebSignature.Payload payload;
+
+                try
+                {
+                    payload = await GoogleJsonWebSignature.ValidateAsync(authUserProfileReceivingDTO.IdToken);
+                }
+                catch (InvalidJwtException invalidJwtException)
+                {
+                    _logger.LogWarning(JsonConvert.SerializeObject(authUserProfileReceivingDTO));
+                    _logger.LogWarning($"Could not validate Google Id Token {authUserProfileReceivingDTO.IdToken} => {invalidJwtException.Message}");
+                    return StatusCode(404, invalidJwtException.Message);
+                }
+
+                if (!payload.EmailVerified)
+                {
+                    _logger.LogWarning($"Email verification failed. Payload => {JsonConvert.SerializeObject(payload)}");
+                    return StatusCode(404, "Email verification failed.");
+                }
+
+                var userProfileDTO = authUserProfileReceivingDTO.UserProfile;
+
+                userProfileDTO.RoleId = userProfileDTO.IsSuperAdmin() ? 1 : 2;
+
+                var response = await userProfileService.AddUserProfile(userProfileDTO);
+
+                if (response.StatusCode >= 400)
+                {
+                    _logger.LogWarning($"Could not create user [{userProfileDTO.Email}] => {response.Message}");
+                    return StatusCode(response.StatusCode, response);
+                }               
+
+                var user = ((ApiOkResponse)response).Result;
+                var userProfile = (UserProfileTransferDTO)user;
+
+                var token = GenerateToken(userProfile);
+                UserAuthTransferDTO userAuthTransferDTO = new UserAuthTransferDTO()
+                {
+                    Token = token,
+                    UserProfile = userProfile
+                };
+
+                return Ok(userAuthTransferDTO);
             }
-            catch (InvalidJwtException invalidJwtException)
+            catch (Exception ex)
             {
-                return StatusCode(404, invalidJwtException.Message);
-            }
-                
-            if (!payload.EmailVerified)
-            {
-                return StatusCode(404, "Email verification failed.");
-            }
-
-            var userProfileDTO = authUserProfileReceivingDTO.UserProfile;
-
-            userProfileDTO.RoleId = userProfileDTO.IsSuperAdmin() ? 1 : 2;
-
-            var response = await userProfileService.AddUserProfile(userProfileDTO);
-
-            if (response.StatusCode >= 400)
-                return StatusCode(response.StatusCode, response);
-
-            var user = ((ApiOkResponse)response).Result;
-            var userProfile = (UserProfileTransferDTO)user;
-
-            var token = GenerateToken(userProfile);
-            UserAuthTransferDTO userAuthTransferDTO = new UserAuthTransferDTO()
-            {
-                Token = token,
-                UserProfile = userProfile
-            };
-
-            return Ok(userAuthTransferDTO);
+                _logger.LogError(ex.Message);
+                _logger.LogError(ex.StackTrace);
+                return StatusCode(500, $"An error occured, {ex.Message}");
+            }          
         }
 
         private string GenerateToken(UserProfileTransferDTO userProfile)
