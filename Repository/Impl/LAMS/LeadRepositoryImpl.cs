d0ee08581e3f216769103adfd220ef111b2a017d
Israel Amamba <i.amambayahoo.com>
2021-10-22 11:22:26 -1100

    removed todo

--- Repository/Impl/LAMS/LeadRepositoryImpl.cs
+++ Repository/Impl/LAMS/LeadRepositoryImpl.cs
@@ -36,8 +36,6 @@ namespace HaloBiz.Repository.Impl.LAMS
 
         public async Task<IEnumerable<Lead>> FindUserLeads(long userId)
         {
-            //todo remove hardcooded id
-            userId = 47;
             return await _context.Leads.Where(lead => !lead.IsDeleted && !lead.LeadConversionStatus && lead.CreatedById == userId).ToListAsync();
         }
 
