11f8f4cec6953360eb9a9ec9fe268a566524d1b0
Yusuf Bashir <Yusuf Bashir@LAPTOP-JR03U537>
2021-11-22 08:52:50 +0100

    serviceregDTO

---
+++ DTOs/ReceivingDTOs/ServiceRegistrationReceivingDTO.cs
@@ -0,0 +1,27 @@
+﻿using HalobizMigrations.Models;
+using HalobizMigrations.Models.Armada;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaloBiz.DTOs.ReceivingDTOs
+{
+    public class ServiceRegistrationReceivingDTO
+    {
+        public string[] ApplicableArmedEscortTypes { get; set; }
+        public string[] ApplicableVehicleTypes { get; set; }
+        public string[] ApplicablePilotTypes { get; set; }
+        public string[] ApplicableCommanderTypes { get; set; }
+        public long ArmedEscortQuantityRequired { get; set; }
+        public bool RequiresArmedEscort { get; set; }
+        public bool RequiresPilot { get; set; }
+        public long VehicleQuantityRequired { get; set; }
+        public bool RequiresVehicle { get; set; }
+        public long CommanderQuantityRequired { get; set; }
+        public bool RequiresCommander { get; set; }
+        public long ServiceId { get; set; }
+
+        public long PilotQuantityRequired { get; set; }
+    }
+}
