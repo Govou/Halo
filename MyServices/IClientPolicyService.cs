c2dac566aa3745397519ec956b2da3d17da6c25f
Israel Amamba <i.amambayahoo.com>
2022-01-12 12:04:12 +0100

    completed client policy

--- MyServices/IClientPolicyService.cs
+++ MyServices/IClientPolicyService.cs
@@ -10,13 +10,11 @@ namespace HaloBiz.MyServices
 {
     public interface IClientPolicyService
     {
-        Task<ApiCommonResponse> AddClientPolicy(HttpContext context, ClientPolicyReceivingDTO clientPolicyReceivingDTO);
+        Task<ApiCommonResponse> AddClientPolicy(HttpContext context, List<ClientPolicyReceivingDTO> clientPolicyReceivingDTO);
         Task<ApiCommonResponse> GetAllClientPolicies();
         Task<ApiCommonResponse> GetClientPolicyById(long id);
         Task<ApiCommonResponse> FindClientPolicyByContractServiceId(long id);
-        Task<ApiCommonResponse> FindClientPolicyByContractId(long id);
-        //Task<ApiCommonResponse> GetClientPolicyByName(string name);
-        Task<ApiCommonResponse> UpdateClientPolicy(HttpContext context, long id, ClientPolicyReceivingDTO clientPolicyReceivingDTO);
+        Task<ApiCommonResponse> UpdateClientPolicy(HttpContext context, List<ClientPolicyReceivingDTO> clientPolicyReceivingDTO);
         Task<ApiCommonResponse> DeleteClientPolicy(long id);
     }
 }
