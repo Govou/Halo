fc15878cc0db359fc2450ff3092f01f32e143eac
bigsam11 <jrmhaustin@gmail.com>
2022-02-03 07:35:05 +0100

    Added new updates

--- DTOs/ProjectManagementDTO/TaskBarChartDTO.cs
+++ DTOs/ProjectManagementDTO/TaskBarChartDTO.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace HaloBiz.DTOs.ProjectManagementDTO
 {
     public class TaskBarChartDTO
     {
-        public TaskBarChartDTO()
-        {
-        }
+        public string TaskOwnerName { get; set; }
+        public int TaskCount { get; set; }
     }
 }
--- DTOs/ProjectManagementDTO/WorkspaceBarchartDTO.cs
+++ DTOs/ProjectManagementDTO/WorkspaceBarchartDTO.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace HaloBiz.DTOs.ProjectManagementDTO
 {
     public class WorkspaceBarchartDTO
     {
-        public WorkspaceBarchartDTO()
-        {
-        }
+        public List<string> WorkspaceName { get; set; }
+
+        public List<long> TaskCount { get; set; }
     }
 }
