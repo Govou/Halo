85aa2c96dda37520bff936e66bcb5868124b3500
Kehinde Guvoeke <Govou11@outlook.com>
2022-05-09 12:46:53 +0100

    added state feature

---
+++ OnlinePortalBackend/MyServices/IUtilityService.cs
@@ -0,0 +1,12 @@
+﻿using Halobiz.Common.DTOs.ApiDTOs;
+using OnlinePortalBackend.DTOs.TransferDTOs;
+using System.Threading.Tasks;
+
+namespace OnlinePortalBackend.MyServices
+{
+    public interface IUtilityService
+    {
+        Task<ApiCommonResponse> GetStates();
+        Task<ApiCommonResponse> GetLocalGovtAreas(int stateId);
+    }
+}
