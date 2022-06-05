0b585650830cd259e2b9df52d1af8cb1e0130d0d
Israel Amamba <i.amambayahoo.com>
2022-06-05 13:55:34 +0100

    advance payment

---
+++ HaloBiz/Controllers/AccountsModel/AdvancePaymentController.cs
@@ -0,0 +1,36 @@
+﻿using Halobiz.Common.Auths;
+using Halobiz.Common.DTOs.ApiDTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaloBiz.Controllers.AccountsModel
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [ModuleName(HalobizModules.Finance, 127)]
+    public class AdvancePaymentController : Controller
+    {
+        [HttpGet("")]
+        public async Task<ApiCommonResponse> GetAdvancePayments()
+        {
+                return await _invoiceService.GetAllInvoice();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ApiCommonResponse> GetByCustomerId(long id)
+        {
+            return await _invoiceService.GetAllInvoicesById(id);
+        }
+        [HttpPut("{id}")]
+        public async Task<ApiCommonResponse> Update(long id)
+        {
+            return await _invoiceService.SendInvoice(invoiceId);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ApiCommonResponse> Post(long id)
+        {
+            return await _invoiceService.SendInvoice(invoiceId);
+        }
+    }
+}
---
+++ HaloBiz/MyServices/Impl/AdvancePaymentService.cs
@@ -0,0 +1,31 @@
+﻿using Halobiz.Common.DTOs.ApiDTOs;
+using HalobizMigrations.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace HaloBiz.MyServices.Impl
+{
+    public class AdvancePaymentService
+    {
+        private readonly HalobizContext _context;
+        private readonly ILogger<AdvancePaymentService> _logger;
+        public AdvancePaymentService(HalobizContext context, ILogger<AdvancePaymentService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<ApiCommonResponse> AddPayment(HttpContext context, AdvancePayments bankReceivingDTO)
+        {
+            bank.CreatedById = context.GetLoggedInUserId();
+            var savedBank = await _context.Advan.SaveBank(bank);
+            if (savedBank == null)
+            {
+                return CommonResponse.Send(ResponseCodes.FAILURE, null, "Some system errors occurred");
+            }
+            var bankTransferDTO = _mapper.Map<BankTransferDTO>(bank);
+            return CommonResponse.Send(ResponseCodes.SUCCESS, bankTransferDTO);
+        }
+    }
+}
